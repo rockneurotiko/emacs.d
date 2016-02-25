cd60e9b29a5b37f1856fd9b12f0b167a3a169526
mgarcia <miguelglafuente@gmail.com>
2016-02-25 12:46:15 +0100

    LaTeX

--- settings/latex-settings.el
+++ settings/latex-settings.el
@@ -1,31 +1,32 @@
-;-----------;
+;;-----------;
 ;;; LaTeX ;;;
-;-----------;
+;;-----------;
 
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
 (include-plugin "auctex")
 (load "auctex.el" -1 1 1)
 (load "preview-latex.el" -1 1 1)
 
+;; Sometimes I do care about MAC, but usually I don't give a fuck
 (if (system-is-mac)
     (progn
       (require 'tex-site)
       (require 'font-latex)
       (setq TeX-view-program-list
-	    (quote 
-	     (("Skim" 
+	    (quote
+	     (("Skim"
 	       (concat "/Applications/Skim.app/Contents/SharedSupport/displayline"
 		       " %n %o %b")))))
-      (setq TeX-view-program-selection 
-	    (quote (((output-dvi style-pstricks) "dvips and gv") 
-		    (output-dvi "xdvi") 
+      (setq TeX-view-program-selection
+	    (quote (((output-dvi style-pstricks) "dvips and gv")
+		    (output-dvi "xdvi")
 		    (output-pdf "Skim")
 		    (output-html "xdg-open")))))
 
   (if (system-is-linux)
-      (setq TeX-view-program-selection 
-	     (quote (((output-dvi style-pstricks) "dvips and gv") 
-		     (output-dvi "xdvi") 
+      (setq TeX-view-program-selection
+	     (quote (((output-dvi style-pstricks) "dvips and gv")
+		     (output-dvi "xdvi")
 		     (output-pdf "evince")
 		     (output-html "xdg-open"))))))
 
