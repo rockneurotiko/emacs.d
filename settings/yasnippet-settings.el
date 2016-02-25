00ffbd5387a4d887149b3d18a5298d35f758fa3d
mgarcia <miguelglafuente@gmail.com>
2016-02-25 11:55:48 +0100

    yasnippet

--- settings/yasnippet-settings.el
+++ settings/yasnippet-settings.el
@@ -1,15 +1,19 @@
-;---------------;
+;;---------------;
 ;;; yasnippet ;;;
-;---------------;
-(package-require 'yasnippet)
+;;---------------;
 
 (add-to-list 'load-path "~/.emacs.d/es6-snippets")
 (require 'es6-snippets)
 
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand)
 
-(yas-global-mode 1)
+(use-package yasnippet
+  :ensure t
+  :diminish yasnippet-mode
+  :config
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand)
+  (yas-global-mode 1))
+
 
 (provide 'yasnippet-settings)
