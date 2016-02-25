fb658a5331028fc98f887f826ac0740d135aabe0
mgarcia <miguelglafuente@gmail.com>
2016-02-25 12:27:57 +0100

    Pony

--- settings/pony-settings.el
+++ settings/pony-settings.el
@@ -1,10 +1,11 @@
-(require 'ponylang-mode)
-
-(add-hook 'ponylang-mode-hook
-          (lambda ()
+(use-package ponylang-mode
+  :ensure t
+  :config
+  (add-hook 'ponylang-mode-hook
+            (lambda ()
               (setq auto-indent-newline-function 'newline-and-indent)
               (setq tab-width 2)
               (auto-indent-mode -1)
-              (electric-indent-local-mode -1)))
+              (electric-indent-local-mode -1))))
 
 (provide 'pony-settings)
