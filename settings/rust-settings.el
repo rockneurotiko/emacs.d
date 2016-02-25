c9b14f7832a7885e3d09abe52c8322e2db1a783a
mgarcia <miguelglafuente@gmail.com>
2016-02-25 12:13:18 +0100

    Rust

--- settings/rust-settings.el
+++ settings/rust-settings.el
@@ -1,17 +1,23 @@
-(package-require 'flycheck)
+;; (package-require 'flycheck)
+
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 ;; (require 'flymake-rust)
 ;; (add-hook 'rust-mode-hook 'flymake-rust-load)
 
-(setq racer-cmd "/usr/bin/racer")
-(setq racer-rust-src-path "/usr/src/rust/src/")
-
-(add-hook 'rust-mode-hook
-          '(lambda ()
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq racer-cmd "/usr/bin/racer")
+  (setq racer-rust-src-path "/usr/src/rust/src/")
+  :config
+  (add-hook 'rust-mode-hook
+            '(lambda ()
                (racer-activate)
                (racer-turn-on-eldoc)
                (local-set-key (kbd "M-.") #'racer-find-definition)
-               (local-set-key (kbd "TAB") #'racer-complete-or-indent)))
+               (local-set-key (kbd "TAB") #'racer-complete-or-indent))))
+
+
 
 (provide 'rust-settings)
