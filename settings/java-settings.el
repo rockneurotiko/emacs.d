6a1bda5987e01da8437be76e4fe0b7c92e033ea3
Rock Neurotiko <miguelglafuente@gmail.com>
2016-02-25 12:54:06 +0100

    java

--- settings/java-settings.el
+++ settings/java-settings.el
@@ -1,17 +1,28 @@
-(require 'eclim)
-(global-eclim-mode)
+(use-package emacs-eclim
+    :ensure t
+    :config
+    (global-eclim-mode)
+    (require 'eclimd)
+    ;; local help
+    (setq help-at-pt-display-when-idle t)
+    (setq help-at-pt-timer-delay 0.1)
+    (help-at-pt-set-timer)
+    (require 'ac-emacs-eclim-source)
+    (ac-emacs-eclim-config))
 
-(require 'eclimd)
+;; (require 'eclim)
+;; (global-eclim-mode)
 
+;; (require 'eclimd)
 
-;; local help
-(setq help-at-pt-display-when-idle t)
-(setq help-at-pt-timer-delay 0.1)
-(help-at-pt-set-timer)
+;; ;; local help
+;; (setq help-at-pt-display-when-idle t)
+;; (setq help-at-pt-timer-delay 0.1)
+;; (help-at-pt-set-timer)
 
-;; add the emacs-eclim source to auto-complete
-(require 'ac-emacs-eclim-source)
-(ac-emacs-eclim-config)
+;; ;; add the emacs-eclim source to auto-complete
+;; (require 'ac-emacs-eclim-source)
+;; (ac-emacs-eclim-config)
 
 
 (provide 'java-settings)
