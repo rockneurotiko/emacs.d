dbdaeb15ccc2f265e6db7e199073938c163b749e
mgarcia <miguelglafuente@gmail.com>
2016-02-25 12:24:27 +0100

    Prolog

--- settings/prolog-settings.el
+++ settings/prolog-settings.el
@@ -1,5 +1,6 @@
+;; KILL THIS
+
 (if (file-exists-p "/home/rock/Descargas/temp/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
-        (load-file "/home/rock/Descargas/temp/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
-    )
+        (load-file "/home/rock/Descargas/temp/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el"))
 
 (provide 'prolog-settings)
